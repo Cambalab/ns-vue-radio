1373a66894c595a5e0da27f387191eeb79ca399b
Estefi <estefi@camba.coop>
2018-08-22 15:20:09 -0300

    Se agrega el botón para abrir el chat en whatsapp con ahijuna

--- src/components/Escribinos.vue
+++ src/components/Escribinos.vue
@@ -1,26 +1,21 @@
 <template>
-  <Page class="page">
-    <ActionBar class="action-bar" title="Escribinos">
-      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')"/>
-    </ActionBar>
+  <FlexboxLayout flexDirection="column" justifyContent="space-between" backgroundColor="#000028">
+    <Label fontSize="30" color="purple">ESCRIBINOS:</Label>
 
     <StackLayout>
-        <Button class="btn btn-primary" @tap="openwsp()" text="Whatsapp"/>
-
+      <Button class="sci" @tap="openwsp()" :text="'\ue099' " fontSize="70" minHeight="110" marginBottom="40" marginLeft="100" marginRight="100" style="background-color: green" color="white"/>
     </StackLayout>
-
-  </Page>
+  </FlexboxLayout>
 </template>
-
 <script>
   import { openApp } from "nativescript-open-app";
+  import { openUrl } from "tns-core-modules/utils/utils";
 
   var appAvailability = require("nativescript-appavailability");
-
   function openwsp () {
     appAvailability.available("com.whatsapp").then(function(avail) {
       if (avail) {
-        openApp("https://wa.me/5491157995025");
+        openUrl("https://wa.me/5491164472428");
       }else{
         openApp("com.whatsapp");
       }
@@ -28,10 +23,11 @@
   }
 
   export default {
-    data () {
+    data: () => {
       return {
         openwsp,
       };
     },
   };
+
 </script>
