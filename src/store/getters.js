512fb322521e835764ee61a555ff84a823a83afb
Facundo Mainere <fmmainere@gmail.com>
2018-08-23 19:41:28 -0300

    agregada reproduccion de podcasts y su maquetacion inicial, uso de vuex para compartir el estado del reproductor entre los componentes y poder pausar la radio cuando se inicia un podcast y viceversa

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+export const getPlayer = state => {
+  return state.player;
+}
+
+export const getPlayPromise = state => {
+  return state.play_promise;
+}
+
+export const getPlayerScreen = state => {
+  return state.player_screen;
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'nativescript-vue';
 import Vuex from 'vuex';
+import * as mutations from './mutations'
+import * as getters from './getters'
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
 
 const store = new Vuex.Store({
-  modules: {
+  state: {
+    player: undefined,
+    player_screen: 'VIVO',
+    play_promise: undefined
   },
-  strict: debug,
-});
+  mutations,
+  getters,
+  strict: debug
+})
+
 
 Vue.prototype.$store = store;
 
