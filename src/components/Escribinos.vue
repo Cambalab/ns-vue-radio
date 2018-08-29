17055fb17470ac8f4f6a505e13f6791f65203bb7
Gabriel Maljkovich <gabriel.l.maljkovich@gmail.com>
2018-08-29 19:06:44 -0300

    Maquetacion Escribinos

--- src/components/Escribinos.vue
+++ src/components/Escribinos.vue
@@ -1,12 +1,22 @@
 <template>
-  <FlexboxLayout flexDirection="column" justifyContent="space-between" backgroundColor="#000028">
-    <Label fontSize="30" color="purple">ESCRIBINOS:</Label>
-
-    <StackLayout>
-      <Button class="sci" @tap="openEmail()" :text="'\ue050'" fontSize="80" marginBottom="30" marginLeft="100" marginRight="100" style="background-color: gray" color="black"/>
-      <Button class="sci" @tap="openWsp()" :text="'\ue099' " fontSize="70" minHeight="110" marginBottom="40" marginLeft="100" marginRight="100" style="background-color: green" color="white"/>
+  <ScrollView class="blue text-center">
+    <StackLayout padding="10">
+      <Label fontSize="22" class="m-t-25 bold" color="white" opacity="0.5">Escribinos</Label>
+      <Label fontSize="14" fontWeight="300" textWrap="true" opacity="0.7" class="m-y-10" color="white" >Envianos un mensaje por email o whatsapp haciendo click en los botones de abajo.</Label>
+      <StackLayout class="text-left" orientation="horizontal" margin="20 0 10" padding="10 20" width="260" color="white" backgroundColor="darkblue">
+        <Label class="sci" @tap="openEmail()" :text="'\ue050'" color="white" backgroundColor="darkblue" fontSize="36" verticalAlignment="center"></Label>
+        <Label fontSize="12" class="bold" padding="0 10" width="170" verticalAlignment="center">Contactanos vía email</Label>
+        <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
+      </StackLayout>
+      <StackLayout orientation="horizontal" class="text-left" margin="10 0" padding="10 20" width="260" color="white" backgroundColor="green">
+        <Label class="sci" @tap="openWsp()" :text="'\ue099' " fontSize="36" color="white" backgroundColor="green" verticalAlignment="center"></Label>
+        <Label fontSize="12" class="bold" padding="0 10" width="170" verticalAlignment="center">Contactanos vía Whatsapp</Label>
+        <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
+      </StackLayout>
+      <!-- <Button class="sci" @tap="openEmail()" :text="'\ue050'" fontSize="80" marginBottom="30" marginLeft="100" marginRight="100" style="background-color: gray" color="black"/> -->
+      <!-- <Button class="sci" @tap="openWsp()" :text="'\ue099' " fontSize="70" minHeight="110" marginBottom="40" marginLeft="100" marginRight="100" style="background-color: green" color="white"/> -->
     </StackLayout>
-  </FlexboxLayout>
+  </ScrollView>
 </template>
 <script>
   import { openApp } from "nativescript-open-app";
