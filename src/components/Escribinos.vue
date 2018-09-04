70a515d521d194eea6ae700ee191c7c1a735167b
Gabriel Maljkovich <gabriel.l.maljkovich@gmail.com>
2018-09-04 16:19:54 -0300

    retoque chiquitito de la UI
    - estas abusando del FlexboxLayout, no es paracetamol que cura todo.
    - Estaba bastante bien la pantalla :D
    - Arregle los botones de la pantalla Escribinos que yo arruine

--- src/components/Escribinos.vue
+++ src/components/Escribinos.vue
@@ -3,13 +3,13 @@
     <StackLayout padding="10">
       <Label fontSize="22" class="m-t-25 bold" color="white" opacity="0.5">Escribinos</Label>
       <Label fontSize="14" fontWeight="300" textWrap="true" opacity="0.7" class="m-y-10" color="white" >Envianos un mensaje por email o whatsapp haciendo click en los botones de abajo.</Label>
-      <StackLayout class="text-left" orientation="horizontal" margin="20 0 10" padding="10 20" width="260" color="white" backgroundColor="darkblue">
-        <Label class="sci" @tap="openEmail()" :text="'\ue050'" color="white" backgroundColor="darkblue" fontSize="36" verticalAlignment="center"></Label>
-        <Label fontSize="12" class="bold" padding="0 10" width="170" verticalAlignment="center">Contactanos vía email</Label>
+      <StackLayout @tap="openEmail()" class="text-left" orientation="horizontal" margin="20 0 10" padding="10 20" width="260" color="darkblue" backgroundColor="#eee">
+        <Label class="sci" :text="'\ue050'" color="darkblue" backgroundColor="#eee" fontSize="36" verticalAlignment="center"></Label>
+        <Label fontSize="12" class="bold" padding="0 10" color="#333" width="170" verticalAlignment="center">Contactanos vía email</Label>
         <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
       </StackLayout>
-      <StackLayout orientation="horizontal" class="text-left" margin="10 0" padding="10 20" width="260" color="white" backgroundColor="green">
-        <Label class="sci" @tap="openWsp()" :text="'\ue099' " fontSize="36" color="white" backgroundColor="green" verticalAlignment="center"></Label>
+      <StackLayout @tap="openWsp()" orientation="horizontal" class="text-left" margin="10 0" padding="10 20" width="260" color="white" backgroundColor="green">
+        <Label class="sci" :text="'\ue099' " fontSize="36" color="white" backgroundColor="green" verticalAlignment="center"></Label>
         <Label fontSize="12" class="bold" padding="0 10" width="170" verticalAlignment="center">Contactanos vía Whatsapp</Label>
         <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
       </StackLayout>
