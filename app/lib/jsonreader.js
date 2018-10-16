75b61ab4c6d4f3967c15576105c8cc8cc55040f5
alejandrok <alejandro@camba.coop>
2018-10-16 12:26:36 -0300

    json to js var to export

--- app/lib/jsonreader.js
+++ app/lib/jsonreader.js
@@ -1,69 +1,40 @@
-import * as fs from "file-system";
-
-var documents = fs.knownFolders.currentApp();
-var jsonFile = documents.getFile('./resources/data.json');
-var array;
-var jsonData;
+import data from './resources/data'
 
 class jsonreader {
     constructor() {
-        this.jsonData = {}
-        this.array = []
-        jsonFile.readText()
-            .then(function (content) {
-                try {
-                    jsonData = JSON.parse(content);
-                    array = new observableArrayModule.ObservableArray(jsonData);
-                    console.log(jsonData)
-                    console.log(array)
-                } catch (err) {
-                    throw new Error('Could not parse JSON file');
-                }
-            }, function (error) {
-                throw new Error('Could not read JSON file');
-            });
+        this.data = data
     }
 
     get(id) {
-        let data = []
-        switch (id) {
-            case "/programas/api":
-                console.log('switch', this.array)
-                data = 
-                data = 
-                    {
+        let res = [];
+        switch (true) {
+            case id.includes('api'):
+                res = {
                         status : 200,
-                        // data : this.array.programas
-                        data : 
-                        [
-                            {
-                                "title": "Señal de ajuste",
-                                "horario": "Lunes a viernes a las 9",
-                                "content": "Una propuesta periodística para arrancar la mañana con la mejor música. Conduce: Martín Iglesias",
-                                "hora_inicio": "09:00:00",
-                                "hora_fin": "10:00:00",
-                                "dias": "[u'1', u'2', u'3', u'4', u'5']",
-                                "image": "http://www.ahijuna.fm/media/se%C3%B1al.jpg"
-                            },
-                            {
-                                "title": "Comunidad Imaginada",
-                                "horario": "Lunes a las 18",
-                                "content": "una propuesta para analizar la realidad internacional desde una visión latinoamericana",
-                                "hora_inicio": "18:00:00",
-                                "hora_fin": "20:00:00",
-                                "dias": "[u'1']",
-                                "image": "http://www.ahijuna.fm/media/comunidad_imaginada.jpg"
-                            }
-                        ]
-                        // */
-                    }
-                
+                        data : this.data.programas
+                }
                 break;
-        
+            case id.includes('progactual'):
+                    res = {
+                    status : 200,
+                    data : this.data.progactual
+                }
+                break;
+                
+            case id.includes('podcasts'):
+                data = {
+                    status : 200,
+                    data : this.data.podcasts
+                }
+                break;
             default:
+                res = {
+                    status : 200,
+                    data : {}
+                }
                 break;
         }
-        return Promise.resolve(data)
+        return Promise.resolve(res)
     }
 
 }
---
+++ app/lib/resources/data.js
@@ -0,0 +1,44 @@
+const data = {
+    "programas" : [
+        {
+            "title": "Señal de ajuste",
+            "horario": "Lunes a viernes a las 9",
+            "content": "Una propuesta periodística para arrancar la mañana con la mejor música. Conduce: Martín Iglesias",
+            "hora_inicio": "09:00:00",
+            "hora_fin": "10:00:00",
+            "dias": "[u'1', u'2', u'3', u'4', u'5']",
+            "image": "http://www.ahijuna.fm/media/se%C3%B1al.jpg"
+        },
+        {
+            "title": "Comunidad Imaginada",
+            "horario": "Jueves a las 18",
+            "content": "una propuesta para analizar la realidad internacional desde una visión latinoamericana",
+            "hora_inicio": "18:00:00",
+            "hora_fin": "20:00:00",
+            "dias": "[u'4']",
+            "image": "http://www.ahijuna.fm/media/comunidad_imaginada.jpg"
+        }
+    ],
+    "podcasts": [
+        {
+            "title": "#Salud | Camila Lynn: \"queremos que termine el recorte y que no se abandone a las provincias\"",
+            "content": "<p>Miles de persoas se movilizaron&nbsp;desde el ex ministerio hasta Plaza de Mayo. Hablamos con la Secretaria de G&eacute;nero de ATE Salud, que comparte los problemas que trae el desfinanciamiento de programas sociales.</p>\r\n\r\n<p><iframe frameborder=\"0\" height=\"30px\" scrolling=\"no\" src=\"https://archive.org/embed/MarchaFederalEnDefensaDeLaSaludPublica\" width=\"100%\"></iframe></p>\r\n",
+            "file_download": "https://archive.org/download/MarchaFederalEnDefensaDeLaSaludPublica/2018.10.04%20-%20Camila%20Lynn%20-%20Sec%20de%20Genero%20ATE%20Salud%20-%20Marcha%20Federal%20en%20defensa%20de%20la%20salud%20publica",
+            "is_published": true,
+            "pub_date": "2018-10-05T15:30:00Z"
+        },
+        {
+            "title": "Cablevisión eliminó el canal PAKA Paka de su servicio básico | Un paso más del vaciamiento",
+            "content": "<p>Sebasti&aacute;n Struciat, delegado de SATSAID comparte&nbsp;su preocupaci&oacute;n por la decisi&oacute;n de la empresa de sacar de su grilla a la se&ntilde;al&nbsp;infantil del Estado. Adem&aacute;s, afirma que disminuy&oacute; dr&aacute;sticamente la producci&oacute;n de contenidos propios y crecieron los programas importados.</p>\r\n\r\n<p><iframe frameborder=\"0\" height=\"30px\" scrolling=\"no\" src=\"https://archive.org/embed/CablevisionEliminoPakaPakaDelServicioBasico\" width=\"100%\"></iframe></p>\r\n",
+            "file_download": "https://archive.org/download/CablevisionEliminoPakaPakaDelServicioBasico/2018.10.03%20-%20Sebastian%20Japones%20Struciat%20-%20Delegado%20de%20Satsaid%20-%20Cablevision%20elimino%20la%20se%C3%B1al%20de%20Paka%20Paka",
+            "is_published": true,
+            "pub_date": "2018-10-05T15:20:00Z"
+        }
+
+    ],
+    "progactual": {
+        "programa": "Error de carga"
+    } 
+}
+
+export default data
