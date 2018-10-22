e62120d898fbe9effc5224803c22cc56cf378f46
Estefi <estefi@camba.coop>
2018-10-22 18:28:05 -0300

    I move hardcoded social networks to the config file

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
 import Vue from 'nativescript-vue'
 import { i18n } from './langs/i18n'
 
-//import App from './components/App'
-//import VueDevtools from 'nativescript-vue-devtools'
+// import VueDevtools from 'nativescript-vue-devtools'
 
 import store from './store'
 
@@ -10,11 +9,11 @@ import './sass/styles.scss'
 
 import Home from './components/Home.vue'
 
-//if(TNS_ENV !== 'production') {
+// if(TNS_ENV !== 'production') {
 //  Vue.use(VueDevtools)
-//}
+// }
 // Prints Vue logs when --env.production is *NOT* set while building
-//Vue.config.silent = (TNS_ENV === 'production')
+// Vue.config.silent = (TNS_ENV === 'production')
 
 Vue.config.silent = false
 
