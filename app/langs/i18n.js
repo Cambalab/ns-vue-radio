72aa06ccdf8bb788b17e6de4fd5a4b423ec0f6c0
Facundo Mainere <fmmainere@gmail.com>
2018-10-26 17:05:04 -0300

    loaded pt lang and added days of the week

--- app/langs/i18n.js
+++ app/langs/i18n.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from './locales/en.json'
 import es from './locales/es.json'
+import pt from './locales/pt.json'
 
 Vue.use(VueI18n)
 
 const locale = 'es'
 const messages = {
   en,
-  es
+  es,
+  pt
 }
 
 export const i18n = new VueI18n({
