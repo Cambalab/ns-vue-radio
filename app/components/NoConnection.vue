b1c4cf6adb2460cfef3d986202c3c40365a75565
Facundo Mainere <fmmainere@gmail.com>
2018-10-30 16:47:55 -0300

    remove uneeded services, they were using the same endpoints and methods

--- app/components/NoConnection.vue
+++ app/components/NoConnection.vue
@@ -14,7 +14,7 @@
     </AbsoluteLayout>
 </template>
 <script>
-import ConnectionService from '../api/ConnectionService'
+import ShowService from '../api/ShowService'
 
 export default {
   data: () => {
@@ -22,7 +22,7 @@ export default {
   },
   methods: {
     updateConnection () {
-      ConnectionService.getCurrentShow().then((resp) => {
+      ShowService.getCurrentShow().then((resp) => {
         if (resp.status !== null) {
           if (!this.connection && this.player.isAudioPlaying()) {
             this.$store.commit('PLAY_URL', this.stream_url)
