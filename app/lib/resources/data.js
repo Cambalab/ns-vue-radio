89637ae6a8f1a4bc6cd654f7671686e695d262bc
Estefi <estefi@camba.coop>
2018-11-01 15:20:48 -0300

    I tidy the podcasts in the local API

--- app/lib/resources/data.js
+++ app/lib/resources/data.js
@@ -19,11 +19,14 @@ const data = {
   ],
   'podcasts': [
     {
-      'title': '#Trabajo | Juan Cruz Lucero (CEPA): \'un país sin industria es un país para pocos\'',
-      'content': '<p>El Centro de Econom&iacute;a Pol&iacute;tica Argentina present&oacute; un nuevo informe sobre despidos y suspensiones en los primeros ocho meses del a&ntilde;o. Empleos precarios y p&eacute;rdida de 90 mil puestos de trabajo en los &uacute;ltimos tres a&ntilde;os.</p>\r\n\r\n<p><iframe frameborder=\"0\" height=\"30px\" scrolling=\"no\" src=\"https://archive.org/embed/CEPADespidosYSuspensionesen2018\" width=\"100%\"></iframe></p>\r\n',
-      'file_download': 'https://archive.org/download/CEPADespidosYSuspensionesen2018/2018.09.27%20-%20Juan%20Cruz%20Lucero%20-%20Integrante%20Cepa%20-%20Informe%20de%20Cepa.mp3',
-      'is_published': true,
-      'pub_date': '2018-09-28T13:11:00Z'
+      'title': 'Event',
+      'content': '<p>This is a event description.</p>',
+      'file_download': 'https://' // audio file
+    },
+    {
+      'title': 'News',
+      'content': '<p>This is a news description.</p>',
+      'file_download': 'https://' // audio file
     }
   ],
   'progactual': {
