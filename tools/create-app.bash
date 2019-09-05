9993360f75eac0182bc35feb9e85c9f6af22f0eb
Estefi <estefi@camba.coop>
2019-09-05 11:28:48 -0300

    refactored script parameters

--- tools/create-app.bash
+++ tools/create-app.bash
@@ -3,20 +3,24 @@
 # Create a script that given an output directory builds the current application
 # and generates an .aab file in the output dir.
 
-# Params: $1: output directory
+### Parameters ###
+
+OUTPUT_DIRECTORY = $1
+ASSETS = $2
+SPLASH_BACKGROUND_COLOR = $3
 
 ### Assets ###
 
-# Place logo in the assets file
-full_path=$(realpath $0)
-dir_path=$(dirname $full_path)
-mv ./logo-hippie.png $dir_path/../app/assets/images/
+# Place logo in the assets directory
+FULL_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FULL_PATH)
+mv $ASSETS/logo.png $DIR_PATH/../app/assets/images/
 
 # Generating splash screen
-tns resources generate splashes <./splash-image.png> --background <rgb-color>
+tns resources generate splashes $ASSETS/splash.png --background $SPLASH_BACKGROUND_COLOR
 
 # Generating app icon
-tns resources generate icons <./icon-image.png>
+tns resources generate icons $ASSETS/icon.png
 
 ### Build ###
 
@@ -24,4 +28,4 @@ tns resources generate icons <./icon-image.png>
 tns build android --bundle --release --compileSdk 28 --key-store-path <./keystore-name.jks> --key-store-password <keystore-password> --key-store-alias <keystore-alias-name> --key-store-alias-password <keystore-alias-password>
 
 # Generate a .aab file
-tns build android --bundle --release --compileSdk 28 --key-store-path <./keystore-name.jks> --key-store-password <keystore-password> --key-store-alias <keystore-alias-name> --key-store-alias-password <keystore-alias-password> --aab --copy-to $1
+tns build android --bundle --release --compileSdk 28 --key-store-path <./keystore-name.jks> --key-store-password <keystore-password> --key-store-alias <keystore-alias-name> --key-store-alias-password <keystore-alias-password> --aab --copy-to $OUTPUT_DIRECTORY
