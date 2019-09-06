56fc43caff85edd401eca5abac0750ed50eee52f
Estefi <estefi@camba.coop>
2019-09-06 10:47:27 -0300

    The script searches for a keystore file and uses it during the tns build

--- tools/create-app.bash
+++ tools/create-app.bash
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Create a script that given an output directory builds the current application
-# and generates an .aab file in the output dir.
-
 ### Parameters ###
 
 OUTPUT_DIRECTORY=$1
 ASSETS_DIRECTORY=$2
 SPLASH_BACKGROUND_COLOR=$3
+KEYSTORE_DIRECTORY=$4
+KEYSTORE_PASS=$5
+KEYSTORE_ALIAS=$6
+KEYSTORE_ALIAS_PASS=$7
 
 ### Assets ###
 
 # Place logo in the assets directory
 FULL_PATH=$(realpath $0)
 DIRECTORY_PATH=$(dirname $FULL_PATH)
-mv $ASSETS_DIRECTORY/logo.png $DIRECTORY_PATH/../app/assets/images/
+cp $ASSETS_DIRECTORY/logo.png $DIRECTORY_PATH/../app/assets/images/
 
 # Generate splash screen
 tns resources generate splashes $ASSETS_DIRECTORY/splash.png --background $SPLASH_BACKGROUND_COLOR
@@ -24,8 +25,8 @@ tns resources generate icons $ASSETS_DIRECTORY/icon.png
 
 ### Build ###
 
-# # Sign app for release
-# tns build android --bundle --release --compileSdk 28 --key-store-path <./keystore-name.jks> --key-store-password <keystore-password> --key-store-alias <keystore-alias-name> --key-store-alias-password <keystore-alias-password>
-#
-# # Generate a .aab file
-# tns build android --bundle --release --compileSdk 28 --key-store-path <./keystore-name.jks> --key-store-password <keystore-password> --key-store-alias <keystore-alias-name> --key-store-alias-password <keystore-alias-password> --aab --copy-to $OUTPUT_DIRECTORY
+# Sign app for release
+tns build android --bundle --release --compileSdk 28 --key-store-path $KEYSTORE_DIRECTORY --key-store-password $KEYSTORE_PASS --key-store-alias $KEYSTORE_ALIAS --key-store-alias-password $KEYSTORE_ALIAS_PASS
+
+# Generate a .aab file
+tns build android --bundle --release --compileSdk 28 --key-store-path $KEYSTORE_DIRECTORY --key-store-password $KEYSTORE_PASS --key-store-alias $KEYSTORE_ALIAS --key-store-alias-password $KEYSTORE_ALIAS_PASS --aab --copy-to $OUTPUT_DIRECTORY
