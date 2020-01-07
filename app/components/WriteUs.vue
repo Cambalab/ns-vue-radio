61cf3c68641e3b69388f54020871f9bb89d850d5
natalia <natalia@camba.coop>
2020-01-07 14:35:06 -0300

    add border in button and change opacity

--- app/components/WriteUs.vue
+++ app/components/WriteUs.vue
@@ -1,14 +1,14 @@
 <template>
   <ScrollView class="text-center" :style="backgroundColor">
     <StackLayout padding="10">
-      <Label fontSize="22" class="m-t-25 bold" :style="primaryTextColor" opacity="0.5">{{ $t('writeUs') }}</Label>
-      <Label fontSize="14" fontWeight="300" textWrap="true" opacity="0.7" class="m-y-10" :style="secondaryTextColor" >{{ $t('sendUs') }}</Label>
-      <StackLayout @tap="openEmail()" class="text-left" orientation="horizontal" margin="20 0 10" padding="10 20" width="260" color="darkblue" backgroundColor="#eee">
+      <Label fontSize="22" class="m-t-25 bold" :style="primaryTextColor" opacity="15">{{ $t('writeUs') }}</Label>
+      <Label fontSize="14" fontWeight="300" textWrap="true" opacity="1" class="m-y-10" :style="secondaryTextColor" >{{ $t('sendUs') }}</Label>
+      <StackLayout @tap="openEmail()" class="text-left" borderRadius="20" orientation="horizontal" margin="20 0 10" padding="10 20" width="260" color="darkblue" backgroundColor="#eee">
         <Label class="sci" :text="'\ue050'" color="darkblue" backgroundColor="#eee" fontSize="36" verticalAlignment="center"></Label>
         <Label fontSize="12" class="bold" padding="0 10" color="#333" width="170" verticalAlignment="center">{{ $t('contactUsViaEmail') }}</Label>
         <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
       </StackLayout>
-      <StackLayout @tap="openWsp()" orientation="horizontal" class="text-left" margin="10 0" padding="10 20" width="260" color="white" backgroundColor="green">
+      <StackLayout @tap="openWsp()" class="text-left" borderRadius="20" orientation="horizontal" margin="10 0" padding="10 20" width="260" color="white" backgroundColor="green">
         <Label class="sci" :text="'\ue099' " fontSize="36" color="white" backgroundColor="green" verticalAlignment="center"></Label>
         <Label fontSize="12" class="bold" padding="0 10" width="170" verticalAlignment="center">{{ $t('contactUsViaWhatsapp') }}</Label>
         <Label fontSize="14" :text="'\ue89e'" class="mdi" horizontalAlignment="right" verticalAlignment="center"></Label>
